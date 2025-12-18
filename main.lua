1c3b9137cc64cd6c474eb5934cc2f419ad2ae28a
Ionut <144262764+Ionutz547@users.noreply.github.com>
2025-12-18 16:24:59 +0000

    update1

--- main.lua
+++ main.lua
@@ -1,32 +1,49 @@
--- [[ CONFIGURARE GENERALA ]] --
-local ScriptName = "Ionut Scripts" 
-local Creator = "Ionut" 
+repeat wait() until game:IsLoaded()
 
--- [[ NOTIFICARE START ]] --
+-- [[ CONFIGURARE ]] --
+local ScriptName = "Ionut Scripts"
+local Creator = "Ionut"
+
+-- Identificare Executor
+local name, version = identifyexecutor()
+print("Executor detectat:", name, "Versiune:", version)
+
+-- Tabel cu link-urile tale de pe GitHub (folosim formatul tau nou)
+local PlaceIds = {
+    [76558904092080] = "https://raw.githubusercontent.com/Ionutz547/AxionScript/main/the_forge.lua",
+    [2788229376] = "https://raw.githubusercontent.com/Ionutz547/AxionScript/main/script_dahood.lua",
+    [155615604] = "https://raw.githubusercontent.com/Ionutz547/AxionScript/main/script_prison.lua",
+}
+
+local currentPlaceId = game.PlaceId
+
+-- Notificare de pornire
 game:GetService("StarterGui"):SetCore("SendNotification", {
     Title = ScriptName,
-    Text = "Se incarca scriptul de " .. Creator .. "...",
+    Text = "Se verifica compatibilitatea pentru " .. Creator .. "...",
     Duration = 5
 })
 
--- [[ LOGICA DE INCARCARE ]] --
-
-if game.PlaceId == 76558904092080 then -- ID-ul pentru jocul The Forge
-    -- Acest link va incarca fisierul the_forge.lua din repository-ul tau
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/Ionutz547/AxionScript/main/the_forge.lua"))()
-
-elseif game.PlaceId == 2788229376 then -- ID pentru Da Hood (Exemplu)
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/Ionutz547/AxionScript/main/script_dahood.lua"))()
-
-elseif game.PlaceId == 155615604 then -- ID pentru Prison Life (Exemplu)
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/Ionutz547/AxionScript/main/script_prison.lua"))()
-
+-- Verificare daca jocul are script in lista
+if PlaceIds[currentPlaceId] then
+    print("Loading script pentru PlaceID: " .. currentPlaceId)
+    
+    -- Incarcare sigura a scriptului (nu da crash daca link-ul e gresit)
+    local success, result = pcall(function()
+        return loadstring(game:HttpGet(PlaceIds[currentPlaceId]))()
+    end)
+    
+    if success then
+        print("✓ " .. ScriptName .. " incarcat cu succes!")
+    else
+        warn("✗ Eroare la incarcarea codului: " .. tostring(result))
+    end
 else
-    -- Daca joci altceva care nu e in lista de mai sus:
+    -- Daca jocul nu e in lista
     game:GetService("StarterGui"):SetCore("SendNotification", {
         Title = "Eroare",
-        Text = "Jocul " .. game.Name .. " nu este suportat inca!",
+        Text = "Joc nesuportat! ID: " .. currentPlaceId,
         Duration = 10
     })
-    warn("Joc nesuportat. ID detectat: " .. game.PlaceId)
+    warn("✗ Nu a fost gasit un script pentru ID-ul: " .. currentPlaceId)
 end
